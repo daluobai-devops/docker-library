efb189887e6800d15af8a0a822b199257ac5366d
wuzhao <wuzhaozhongguo@hotmail.com>
2025-02-10 21:26:12 +0800

    1

--- push.sh
+++ push.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 set -eo pipefail
-imageName=consul
-tagVersion=1.10.3
-platformParam='--platform linux/arm64/v8'
-docker pull "${platformParam}" ${imageName}:${tagVersion}
+imageName=schnitzler/mysqldump
+targetImageName=schnitzler-mysqldump
+tagVersion=latest
+#platformParam='--platform linux/arm64/v8'
+#docker pull "${platformParam}" ${imageName}:${tagVersion}
+docker pull ${imageName}:${tagVersion}
 
-docker tag ${imageName}:${tagVersion} registry.cn-hangzhou.aliyuncs.com/wuzhaozhongguo/${imageName}:${tagVersion}
-docker push registry.cn-hangzhou.aliyuncs.com/wuzhaozhongguo/${imageName}:${tagVersion}
+docker tag ${imageName}:${tagVersion} registry.cn-hangzhou.aliyuncs.com/wuzhaozhongguo/${targetImageName}:${tagVersion}
+docker push registry.cn-hangzhou.aliyuncs.com/wuzhaozhongguo/${targetImageName}:${tagVersion}
