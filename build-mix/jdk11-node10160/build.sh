b282f0ee7b893724835ea333127bc400847bce43
wuzhao <wuzhaozhongguo@hotmail.com>
2025-02-11 15:06:20 +0800

    1

---
+++ build-mix/jdk11-node10160/build.sh
@@ -0,0 +1,7 @@
+set -eo pipefail
+tagVersion=jdk11-node10160
+docker build -t wuzhaozhongguo/build-mix:${tagVersion} .
+docker push wuzhaozhongguo/build-mix:${tagVersion}
+
+docker tag wuzhaozhongguo/build-mix:${tagVersion} registry.cn-hangzhou.aliyuncs.com/wuzhaozhongguo/build-mix:${tagVersion}
+docker push registry.cn-hangzhou.aliyuncs.com/wuzhaozhongguo/build-mix:${tagVersion}
